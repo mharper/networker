fb5e55cf1386874a9f9a5a9dde86f440dab1f7d8
Michael Harper <mharper@standalonecode.com>
2015-10-29 13:46:24 -0700

    Initial Commit

---
+++ Networker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Networker
+//
+//  Created by Michael Harper on 10/29/15.
+//  Copyright © 2015 Standalone Code LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
